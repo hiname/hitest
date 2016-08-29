ad1d835d0a50242e4319d912871dfbc484026c8b
hiname1 <shaderspirit@gmail.com>
2016-08-29 19:57:38 +0900

    abc

--- HiTest/src/HiClass.java
+++ HiTest/src/HiClass.java
@@ -4,10 +4,7 @@ public class HiClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// aa
-<<<<<<< HEAD
 		// bb
-=======
->>>>>>> branch 'master' of https://github.com/hiname/hitest.git
 	}
 
 }
